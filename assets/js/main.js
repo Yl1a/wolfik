3711f82d7275db9b12febb2f8c4b363a0dd16f80
Yl1a <volkova.ylia2004@gmail.com>
2023-09-09 10:07:31 +0300

    calc

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,61 @@
-let first = +prompt("Введите первое число: ");
-let second = +prompt("Введите второе число: ");
+const inputOne = document.querySelector('.inputOne');
+const inputSec = document.querySelector('.inputSec');
+const btnPlus = document.querySelector('.plus');
+const btnMinus = document.querySelector('.minus');
+const btnMulti = document.querySelector('.Multi');
+const btnDivision = document.querySelector('.Division');
+const btnResult = document.querySelector('.btnResult');
+const resultEl = document.querySelector('.result');
 
-if (first === second) {
-    alert("Покупка совершена");
-} else if (first > second) {
-    alert(`Для покупки не хватает: ${first - second}р.`);
-} else {
-    alert(`Покупка совершена! Ваша сдача ${second - first}p.`);
+
+let action = '+'
+
+btnPlus.onclick = function(){
+    action = '+'
 }
+
+btnMinus.onclick = function(){
+    action = '-'
+}
+btnMulti.onclick = function(){
+    action = '*'
+}
+btnDivision.onclick = function(){
+    action = '/'
+}
+
+
+function printResult(result){
+    if(result < 0) {
+        resultEl.style.color = 'red'
+    } else {
+        resultEl.style.color = '#115739'
+        resultEl.textContent = result;
+
+    }
+}
+
+btnResult.onclick = function () {
+    if(action === '+'){
+        const sum = Number(inputOne.value) + Number(inputSec.value);
+        resultEl.textContent = sum;
+        printResult(sum);
+    } else if(action === '-'){
+        const sum = Number(inputOne.value) - Number(inputSec.value);
+        resultEl.textContent = sum;
+        printResult(sum);
+
+    }else if(action === '*'){
+        const sum = Number(inputOne.value) * Number(inputSec.value);
+        resultEl.textContent = sum;
+        printResult(sum);
+
+    }else{
+        const sum = Number(inputOne.value) / Number(inputSec.value);
+        resultEl.textContent = sum;
+        printResult(sum);
+
+    }
+}
+
+
